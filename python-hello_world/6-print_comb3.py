072ad26c856be5f84f1aebdcbd7663ba26978502
Semira Hussien <semutiye22@gmail.com>
2023-11-30 18:34:44 +0300

    python

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,5 @@
 for i in range(10):
     for j in range(i + 1, 10):
-       print(str(i) + str(j), end=", " if i != 8 or j != 9 else "\n")
+       print("{:02d}".format(i * 10 + j), end=(", " if i != 8 or j != 9 else "\n") if i != 9 else "")
   
         
