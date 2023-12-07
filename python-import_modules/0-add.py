f5f47e781749c99837a63dbac208e4fa8e0d8ff1
Semira Hussien <semutiye22@gmail.com>
2023-12-07 11:44:11 +0300

    new

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -7,20 +7,3 @@ if __name__ == "__main__":
 result = add_0.add(a,b)
 print("a = {} and b = {} FAKE add() => {} - {}".format(a,b,a,b))
 
-a = 10
-b = 30
-
-result = add_0.add(a,b)
-print("a = {} and b = {} FAKE add() => {} - {}".format(a,b,a,b))
-
-a = -10
-b = 30
-
-result = add_0.add(a,b)
-print("a = {} and b = {} FAKE add() => {} - {}".format(a,b,a,b))
-
-a = 5
-b = "H"
-
-result = add_0.add(a,b)
-print("a = {} and b = {} FAKE add() => {} - {}".format(a,b,a,b))
