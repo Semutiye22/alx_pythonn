83f4caa6dcfcdc87d28a770fba3857d419d5b28e
Semira Hussien <semutiye22@gmail.com>
2023-12-14 10:00:04 +0300

    hi

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,26 @@
+class BaseGeometry:
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
