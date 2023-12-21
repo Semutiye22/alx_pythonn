e956be7e94248fc686ef6eb75b5e090e9bf84cfe
Semira Hussien <semutiye22@gmail.com>
2023-12-21 20:32:29 +0300

    h

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -1,24 +1,14 @@
-import requests
+"""Check"""
 import sys
+import requests
+from requests.auth import HTTPBasicAuth
 
-if len(sys.argv) < 3:
-    print("Please provide your username and personal access token.")
-    sys.exit(1)
 
-username = sys.argv[1]
-password = sys.argv[2]
-url = 'https://api.github.com/user'
 
-response = requests.get(url, auth=(username, password))
+auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
 
-if response.status_code == 200:
-    user_info = response.json()
-    user_id = user_info.get('id')
-    if user_id:
-        print(f"Your GitHub user ID is: {user_id}")
-    else:
-        print("User ID not found in the response.")
-else:
-    print(f"Failed to fetch user data. Status code: {response.status_code}")
+r = requests.get("https://api.github.com/user", auth=auth)
+
+print(r.json().get("id"))
      
     
