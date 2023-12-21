c4cc38cf9ed63acf7a861433d52eb28b4be38f81
Semira Hussien <semutiye22@gmail.com>
2023-12-21 12:09:36 +0300

    almost a circle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,26 +17,8 @@ class Base:
         Args:
             id (int, optional): Identifier for the object. Defaults to None.
 
-        Examples:
-            Creating an instance of Base without passing an id:
-                >>> obj1 = Base()
-                >>> print(obj1.id)
-                1
-
-            Creating an instance of Base by passing an id:
-                >>> obj2 = Base(100)
-                >>> print(obj2.id)
-                100
-
-            Creating an instance of Base without passing an id will assign an id:
-                >>> obj3 = Base()
-                >>> print(obj3.id)
-                2
-
-            Creating an instance of Base without passing an id will assign an id + 1 of the previous instance created:
-                >>> obj4 = Base()
-                >>> print(obj4.id)
-                3
+        If provided, sets the id directly to the given value.
+        If not provided, increments the __nb_objects count and assigns the new value as id.
         """
         if id is not None:
             self.id = id  # assign the public instance attribute id with the argument value
