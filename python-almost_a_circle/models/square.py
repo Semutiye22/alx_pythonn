6e504ed581b77fd2db6eafb9d93518943e3e4248
Semira Hussien <semutiye22@gmail.com>
2023-12-21 20:41:34 +0300

    hello

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,45 +1,48 @@
-""" importing Rectangle """
+""" squares """
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
-    """ Square class """
-    def init(self, size, x=0, y=0, id=None):
-        """ initializes empty Square """
-        super().init(size, size, x, y, id)
+    """
+    Square class inheriting from Rectangle.
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initializes a Square object.
+
+        Args:
+            size (int): Size of the square.
+            x (int, optional): x-coordinate of the square. Defaults to 0.
+            y (int, optional): y-coordinate of the square. Defaults to 0.
+            id (int, optional): Identifier for the object. Defaults to None.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than or equal to 0.
+        """
+        super().__init__(size, size, x, y, id)  # Calls the superclass constructor with required arguments
 
     @property
     def size(self):
-        """ finds size """
+        """
+        Getter for size attribute.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ validates size is same as width and height """
+        """
+        Setter for size attribute.
+
+        Args:
+            value (int): The value to set for the size.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
         self.width = value
         self.height = value
-
-    def str(self):
-        """ prints the unoffical string representation of square """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}"\
-            .format(self.id, self.x, self.y, self.width)
-
-    def update(self, *args, **kwargs):
-        """ assigns attributes to square """
-        if len(args) != 0:
-            i = 0
-            square_attrs = ["id", "size", "x", "y"]
-            for arg in args:
-                setattr(self, square_attrs[i], args[i])
-                i += 1
-        else:
-            for k, v in kwargs.items():
-                setattr(self, k, v)
-
-    def to_dictionary(self):
-        """ returns dictionary representation of square """
-        square_dict = {}
-        square_attrs = ["id", "size", "x", "y"]
-        for attr in square_attrs:
-            square_dict[attr] = getattr(self, attr)
-        return square_dict
