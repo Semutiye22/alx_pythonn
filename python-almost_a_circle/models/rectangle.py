c40e9c0f1d2abf2c79bf8e5138ab9ffc43476260
Semira Hussien <semutiye22@gmail.com>
2023-12-21 12:02:02 +0300

    almost a circle

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,27 @@
 from models.base import Base  # Importing the Base class
 
 class Rectangle(Base):
+    """
+    Rectangle class inheriting from Base.
+
+    Attributes:
+        width (int): Width of the rectangle.
+        height (int): Height of the rectangle.
+        x (int): x-coordinate of the rectangle.
+        y (int): y-coordinate of the rectangle.
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle object.
+
+        Args:
+            width (int): Width of the rectangle.
+            height (int): Height of the rectangle.
+            x (int, optional): x-coordinate of the rectangle. Defaults to 0.
+            y (int, optional): y-coordinate of the rectangle. Defaults to 0.
+            id (int, optional): Identifier for the object. Defaults to None.
+        """
         super().__init__(id)  # Call the superclass constructor with id
         self.width = width
         self.height = height
@@ -15,6 +35,12 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        """
+        Sets the width of the rectangle.
+
+        Args:
+            value (int): Width value to set.
+        """
         if value > 0:
             self.__width = value
 
@@ -24,6 +50,12 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        """
+        Sets the height of the rectangle.
+
+        Args:
+            value (int): Height value to set.
+        """
         if value > 0:
             self.__height = value
 
@@ -33,6 +65,12 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        """
+        Sets the x-coordinate of the rectangle.
+
+        Args:
+            value (int): x-coordinate value to set.
+        """
         self.__x = value
 
     @property
@@ -41,4 +79,10 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        """
+        Sets the y-coordinate of the rectangle.
+
+        Args:
+            value (int): y-coordinate value to set.
+        """
         self.__y = value
