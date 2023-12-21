e74169d45272668639d2c7ff4f76ceba1ec619ac
Semira Hussien <semutiye22@gmail.com>
2023-12-21 11:10:12 +0300

    network

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,12 +1,19 @@
-''' htbn status'''
 import requests
 
-url = 'https://alu-intranet.hbtn.io/status'
-response = requests.get(url)
+def fetch_status():
+    """
+    Fetches data from https://intranet.hbtn.io/status using the requests library.
+    Displays the response body in a tabulated format.
+    """
+    url = 'https://intranet.hbtn.io/status'
+    response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
-    for key, value in data.items():
-        print(f"{key}:\n{' ' * 4}{value}")
-else:
-    print(f"Failed to fetch data. Status code: {response.status_code}")
+    if response.status_code == 200:
+        data = response.json()
+        for key, value in data.items():
+            print(f"\t- {key}: {value}")
+    else:
+        print(f"Failed to fetch data. Status code: {response.status_code}")
+
+# Execution starts here
+fetch_status()
